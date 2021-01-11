7aa96198a6f9200dd6dc20ec7ea46e0076166fbf
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2021-01-11 14:53:39 +0100

    move and modification todo comment, move header tag, add css stylesheet and bulma css link

--- includes/chefProfile.php
+++ includes/chefProfile.php
@@ -1,3 +1,5 @@
+<!--TODO: move head, header & footer to a more general page. "Profile" page and include specific profiles to that one page ? -->
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,6 +7,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
     <title>Personal page</title>
 </head>
 <body>
@@ -14,7 +18,6 @@
         <h1>MyBocuse</h1>
         <!--Profile logo to go ot profile view-->
     </header>
-
 <main>
     <h1>Hello chef @[user]!</h1>
 
--- includes/studentProfile.php
+++ includes/studentProfile.php
@@ -1,3 +1,5 @@
+<!--TODO: move head, header & footer to a more general page. "Profile" page and include specific profiles to that one page ? -->
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,15 +7,17 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
     <title>Personal page</title>
 </head>
-<!--TODO: move header to a more general page. "Profile" page and include specific profiles to that one page ? -->
+<body>
+
 <header class="profileViewHeader">
     <!--Bocuse logo-->
     <h1>MyBocuse</h1>
     <!--Profile logo to go ot profile view-->
 </header>
-<body>
     <main>
         <h1>Hello @[user]!</h1>
 
