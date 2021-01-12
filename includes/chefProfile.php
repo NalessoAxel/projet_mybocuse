e42810a6e30f6e0ca529743d6afb7e98489f629b
NalessoAxel <axelnalesso@gmail.com>
2021-01-12 13:22:44 +0100

    add assets and include in chefprofil.php

--- includes/chefProfile.php
+++ includes/chefProfile.php
@@ -12,54 +12,11 @@
     <title>Personal page</title>
 </head>
 <body>
-    <!--TODO: move header to a more general page. "Profile" page and include specific profiles to that one page ? -->
-    <header class="profileViewHeader">
-        <!--Bocuse logo-->
-        <p>MyBocuse</p>
-        <!--Profile logo to go ot profile view-->
-    </header>
-<main>
-    <p>Hello chef @[user]!</p>
-
-    <div class="attendances">
-        <p class="attendancesTitle">Attendances</p>
-        <p>You can consult student attendances here.</p>
-        <div class="studentAttendanceDiv">
-            <p class="studentName">[STUDENT NAME]</p>
-            <div class="studentAttendance">
-                <div class="arrivalDiv">
-                    <p class="arrivalTitle">Arrival</p>
-                    <p class="arrivalTime">[CHECK IN TIME]</p>
-                </div>
-                <div class="DepartureDiv">
-                    <p class="departureTitle">Arrival</p>
-                    <p class="depatureTime">[CHECK OUT TIME]</p>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="studentProfiles">
-        <p>Access to student profiles</p>
-
-        <div class="studentProfileCard">
-            <!--student image-->
-            <p>[STUDENT NAME]</p>
-            <!--Profile button to go to profile-->
-        </div>
-    </div>
+<?php include 'navbar.php'; ?>
 
 
-    <div class="myRecipes">
-        <p class="myRecipesTitle">My Recipes</p>
-        <p>There are [NUMBER] recipes so far:</p>
-        <div class="recipesTitle">[Recipe Title 1]</div>
-        <div class="recipesTitle">[Recipe Title 2]</div>
-        <div class="recipesTitle">[Recipe Title 3]</div>
-        <button class="allRecipes">See all Recipes</button>
-    </div>
 
-</main>
+
+<?php include 'footer.php'; ?>
 </body>
-<footer>BeCode project</footer>
 </html>
