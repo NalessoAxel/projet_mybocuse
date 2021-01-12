845c4984f2fd2945a667bb286463e2ce371a03bb
NalessoAxel <axelnalesso@gmail.com>
2021-01-12 16:57:59 +0100

    front fix missing navbar and footer

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="container is-flex is-justify-content-space-between is-align-items-center">
+      <p class="has-text-white">
+        Fair use disclaimer, this website is for educational purpose only.
+      </p>
+      <figure class="image is-96x96 ">
+        <img src="../assets/becode_logo.svg" alt="logo becode">
+      </figure>
+    </div>
+
+  </footer>
---
+++ includes/navbar.php
@@ -0,0 +1,9 @@
+<nav class="navbar is-flex is-align-items-center" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand is-flex is-justify-content-flex-start is-align-items-center">
+      <figure class="image is-128x128">
+        <img src="../assets/paul_bocuse.jpeg" alt="mybocuse logo">
+      </figure>
+    </div>
+    <h1 class="title is-justify-content-center">My Bocuse</h1>
+    </div>
+  </nav>
