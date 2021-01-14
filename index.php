0a7d2801a4ed4ec6add22fa959e7d6a18a476baa
NalessoAxel <axelnalesso@gmail.com>
2021-01-14 09:44:56 +0100

    Front: Add accordion to All reciepes page + fix css

---
+++ index.php
@@ -0,0 +1,40 @@
+<<<<<<< Updated upstream
+=======
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+  <link rel="stylesheet" href="style.css">
+  <title>My Bocuse</title>
+</head>
+
+<body>
+  <nav class="navbar is-flex is-align-items-center" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand is-flex is-justify-content-flex-start is-align-items-center">
+      <figure class="image is-128x128">
+        <img src="assets/paul_bocuse.jpeg" alt="mybocuse logo">
+      </figure>
+    </div>
+    <h1 class="title is-justify-content-center">My Bocuse</h1>
+    </div>
+  </nav>
+
+
+  <section class="hero">
+    <div class="hero-body is-flex is-flex-direction-column is-align-items-center">
+
+        <img src="assets/img_connexion_page.svg" alt="paul bocuse profil">
+        <a href="./includes/login-form.php"><button class="button  is-rounded mt-5"> CONNEXIONS</button></a>
+      
+    </div>
+
+  </section>
+
+  <?php include 'includes/footer.php'?>
+</body>
+
+</html>
+>>>>>>> Stashed changes
